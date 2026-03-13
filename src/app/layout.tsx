e8ed04d17aa511ceb4c2921ccc432d67922d71af
dhruv270106 <ranadhruv947@gmail.com>
2026-03-13 15:18:25 +0530

    Optimize CSS and Flaticon imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '@flaticon/flaticon-uicons/css/all/all.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
