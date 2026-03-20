4c981101342b4e4d02e42dc5fce7ee484bef1f7c
dhruv270106 <ranadhruv947@gmail.com>
2026-03-20 16:02:39 +0530

    homepage hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import Link from 'next/link'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { useState, useEffect, useRef } from 'react'
 import Navbar from '@/components/Navbar'
